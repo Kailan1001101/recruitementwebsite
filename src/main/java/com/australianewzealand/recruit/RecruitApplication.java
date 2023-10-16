af263ef3da93305f81faaa5c68efb94229dd65b9
kailan1001101 <kailan1001101@gmail.com>
2023-10-16 21:41:41 +0200

    Initial

---
+++ src/main/java/com/australianewzealand/recruit/RecruitApplication.java
@@ -0,0 +1,15 @@
+package com.australianewzealand.recruit;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitApplication.class, args);
+	}
+
+}
